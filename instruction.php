e794cdd20dbd9dbc1eecb463573dc44b96184cba
snehalpingale <65126096+snehalpingale@users.noreply.github.com>
2020-12-30 21:04:33 +0530

    Add files via upload

---
+++ instruction.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Instruction For Online Examination System.</title>
+        <meta name="description" content="An interactive getting started guide for Brackets.">
+        <link rel="stylesheet" href="main.css">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bg {
+  /* The image used */
+  background-image: url("http://www.omnetway.com/sitecontent/uploads/2018/05/banner-smart-exam1.jpg");
+
+  /* Full height */
+  height: 25%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+    </head>
+    <body>
+        <div class="bg"></div>
+        <h1 align="center">ONLINE EXAMINATION INSTRUCTIONS.</h1>
+        
+        <strong>Steps For&nbsp;Accessing Your Exam Online:</strong>
+        
+        <ul>
+                <li>Close all programs, including email</li>
+                <li>Click on the Click here to open the exam link provided in the email from The College.</li>
+                <li>Click "Log In For Your Exam Here" at the bottom of the screen.</li>
+                <li>Have your Proctor enter the Username and Password provided in the email from The College and click enter.</li>
+                <li>To begin the exam, click on the link to the appropriate exam listed under Online Assessments.</li>
+        </ul>
+        <p><br>
+                <strong>Before starting the exam:</strong>
+        </p>
+        
+        <p>Please verify that the student's last name appears correctly within the User ID box.</p>
+        
+        <p><strong>During the exam:</strong></p>
+        <ul>
+            <li>The student may not use his or her textbook, course notes, or receive help from a proctor&nbsp;or any other outside source.</li>
+            <li>Students must complete the 50-question multiple-choice exam within the 75-minute time frame allotted for the exam.</li>
+            <li>Students must not stop the session and then return to it. This is especially important in the online environment where the system will "time-out" and not allow the student or you to reenter the exam site.</li>
+        </ul>
+        
+        <h4><strong>What to do if your online exam is interrupted</strong></h4>
+        
+        <ul>
+	<li>If your online exam is interrupted, click the “Back” button on your web browser to see if you can return to the exam. If not, follow the instructions below to resume taking the exam. <em>Note: Answers are saved by the system in 10-minute intervals. If you have to log back in to complete your exam, your prior answers will be remain from the last system-save.</em></li>
+	<li>Reconnect to the Internet and log back into Brightspace.</li>
+	<li>Follow your original instructions to access the exam login page.</li>
+	<li>Ask your proctor to re-enter the Username and Password, then select the checkbox "Show list of unfinished exams, only select to re-enter an exam already in progress."&nbsp;This will enable you to resume taking the exam, close to the point before the interruption occurred.</li>
+	<li>Have your Proctor enter the Username and Password provided in the email from The College and click enter.</li>
+</ul>
+        
+        
+        <div class="grey-bg">
+      <div class="container">
+        <!-- More Info-->
+        <div class="basic-left-button-row">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <h4 class="cta-title"></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 ">
+                        </div>
+
+            <div class="col-xs-12 col-md-12 text-center" align="center">
+                              <a role="button" class="enroll-button btn" href="http://localhost/PN-exam/index.php">Log In For Your Exam Here</a><br><br>
+                          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </body>
+</html>
+
+  
